e5e66fc1c9b4cccde4d2ac7c3e5a7271a649910f
srabon719 <tisrabon1661@gmail.com>
2026-08-24 09:43:54 +0300

    add second python exercise

---
+++ 2nd.py
@@ -0,0 +1,15 @@
+print("Hello world")
+print('Hello world')
+print("It's a beautiful day")
+print('"moi",said srabon')
+print("Hello")
+print("World")
+print("Hello\nWorld")
+print("Hello\tHello")
+
+name = "srabon"
+add1=id(name)
+print(add1)
+name= "Albert"
+add2=id(name)
+print(add2)
