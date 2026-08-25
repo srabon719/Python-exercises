333d43c3a75dd20b84117a5b2ee3bc820e232c26
srabon719 <tisrabon1661@gmail.com>
2026-08-25 14:56:47 +0300

    class 4

---
+++ condition.py
@@ -0,0 +1,24 @@
+"""
+money = float(input("Enter the amount of money you have: "))
+if (money >= 550):
+ age = int(input("Enter your age: "))
+if (money >=550 and age >=18):
+        print("you can buy a PS5.")
+elif (money >= 550 and age < 18):
+      print("you are not old enough to buy a PS5.")
+else:
+    print("you do not have enough money to buy a PS5.")
+    """
+"""
+age = int(input("Enter your age: "))
+if age >=65:
+     print("You are retired>")
+elif age >=18:
+     print("You are working age")
+elif age >=7:
+     print("you are in school")
+elif age >=3:
+     print("You are preschooler")
+else:
+        print("You are a baby")
+"""
---
+++ grade.py
@@ -0,0 +1,26 @@
+"""
+Grade = float(input("Enter your score: "))
+if Grade >=90:
+   print("You got an A1")
+elif Grade >=80:
+    print("You got an A2")
+elif Grade >=70:
+    print("You got a B1")
+elif Grade >=60:
+    print("You got a B2")
+elif Grade >=50:
+    print("You got a C")
+else:
+    print("You failed the exam")
+"""
+"""
+citizenship = input("Enter if you are a citizen (yes/no): ")
+if citizenship == "yes":
+    age = int(input("Enter your age:"))
+    if age >=18:
+        print("You are eligible to vote.")
+    else:
+        print("You are an underage citizen and not eligible to vote yet.")
+else:
+    print("You are not eligible to vote because you are not a citizen.")
+"""
